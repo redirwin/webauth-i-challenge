550869d9decfcb7b01ce30b856bf46108074579b
Dave Irwin <david@redirwin.com>
2019-08-20 16:52:57 -0700

    Session support working correctly.

--- api/server.js
+++ api/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
 
 const authRouter = require("../auth/auth-router");
 
 const server = express();
 
+const sessionConfig = {
+  name: "session",
+  secret: "kiskis",
+  object: {
+    maxAge: 1000 * 60 * 60 * 3, // lasts for 3 hours
+    secure: false, // set to 'true' in production
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: true // GDPR laws against setting cookies automatically. Set to 'false' in production!
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use("/api/auth", authRouter);
 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -35,6 +35,7 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `${user.username} logged in.` });
       } else {
         res.status(401).json({ message: "You shall not pass!" });
